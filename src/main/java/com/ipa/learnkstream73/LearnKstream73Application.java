9c995d484a41940718ea5abe58a8a030174c06ed
riparraguirre <riparraguirre@marketaxess.com>
2024-10-14 13:05:03 -0300

    first commit

---
+++ src/main/java/com/ipa/learnkstream73/LearnKstream73Application.java
@@ -0,0 +1,13 @@
+package com.ipa.learnkstream73;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnKstream73Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnKstream73Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipa/learnkstream73/LearnKstream73ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipa.learnkstream73;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnKstream73ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
